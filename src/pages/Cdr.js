74c82560a9a429fe6bf48151998cf3d311e5a946
MerseniBilel <bilelmerseni7016@gmail.com>
2021-05-26 16:33:59 +0100

    cdr event handler start

--- src/pages/Cdr.js
+++ src/pages/Cdr.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 
 const Cdr = () => {
+    
+    const [wikendVisible, setwikendVisible] = useState(true)
+    const [events, setevents] = useState([])
+    
+
+    function renderEventContent(eventInfo) {
+        return (
+            <>
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.event.title}</i>
+            </>
+        )
+    }
+
     return (
         <div>
-        
-        <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-        />
-
+            <div className="mt-4">
+                <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                initialView='dayGridMonth'
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                weekends={wikendVisible}
+                initialEvents={events} 
+                eventContent={renderEventContent} 
+            />
+            </div>
         </div>
     )
 }
